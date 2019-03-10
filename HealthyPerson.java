ab133e0182e5d05471bfe79da11b7964996da866
Kenton Huff <kentonahuff@gmail.com>
2019-03-10 00:11:17 -0600

    Implemented HealthyPerson

---
+++ HealthyPerson.java
@@ -0,0 +1,26 @@
+
+public class HealthyPerson extends Person
+{
+	private String reason;
+	
+	public HealthyPerson(String name, int age, String reason)
+	{
+		super(name, age);
+		this.reason = reason;
+	}
+	
+	public int compareToImpl(Person o)
+	{
+		if(!(o instanceof HealthyPerson))
+		{
+			return 0;
+		}
+		
+		return this.getName().compareToIgnoreCase(o.getName());
+	}
+	
+	public String toString()
+	{
+		return String.format("%s In for %s", super.toString(), reason);
+	}
+}
